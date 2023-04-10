2404e9e5aeb5d94a6b185d7a1c1e00c41fe8fbe0
Joseph Francioso <joefrancioso@gmail.com>
2023-04-10 12:29:48 -0400

    Added restaurant card to home until restaurant list is completed

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,11 +2,24 @@
   <div class="home">
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
+     <div class="restaurant-cards">
+      <RestaurantCard
+        v-for="restaurant in restaurants"
+        :key="restaurant.id"
+        :name="restaurant.name"
+        :address="restaurant.address"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import RestaurantCard from '../components/RestaurantCard.vue';
+
 export default {
-  name: "home"
+  name: "Home",
+  components: {
+    RestaurantCard,
+  },
 };
 </script>
