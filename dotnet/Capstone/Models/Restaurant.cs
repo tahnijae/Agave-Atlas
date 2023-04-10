a46303a8a88afd6747be0e6f609f5df80c88c523
Ben Mullaly <benmullaly14@gmail.com>
2023-04-10 20:00:12 +0000

    Update Restaurant.cs

--- dotnet/Capstone/Models/Restaurant.cs
+++ dotnet/Capstone/Models/Restaurant.cs
@@ -1,19 +1,18 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace Capstone.Models
 {
     public class Restaurant
     {
-<<<<<<< HEAD
-        public int restaurant_ID { get; set; }
+
+        
         public string Name { get; set; }
         public string ZipCode { get; set; }
         public List<Drink> Drinks { get; set; }
-=======
+
         public int Restaurant_ID { get; set; }
-        public string Name { get; set; }
-        public string ZipCode { get; set; }
+        
         //public List<Drink> Drinks { get; set; } - Took out for now since getting a restaurant from the database doesn't include
->>>>>>> 65e663621f30135af884833261caa66dc2452fbf
+
     }
 }
