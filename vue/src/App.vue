c712d8c3b082166cb55702fc5a4741a0cbf48440
Ben Mullaly <benmullaly14gmail.com>
2023-04-11 11:03:37 -0400

    going to delete this merge

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,24 +1,39 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link v-if="!isAuthenticated" v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-if="isAuthenticated" v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'login' }" v-if="!isAuthenticated">Login</router-link>&nbsp;|&nbsp;
+      <!-- <router-link v-if="!isAuthenticated" v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp; -->
+      <router-link  v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'login' }" v-show="!isAuthenticated">Login</router-link>&nbsp;|&nbsp;
+      <router-link v-on:click="logout" v-bind:to="{ name: 'logout' }" v-show="isAuthenticated">Logout</router-link>&nbsp;   
     </div>
     <router-view />
   </div>
 </template>
 
 <script>
+//import { computed } from 'vue';
 export default {
   name: "App",
   data() {
     return {
-      isAuthenticated: false, // Set to false by default
+       // Set to false by default
     };
   },
   created() {
     this.isAuthenticated = this.$store.state.token !== "";
   },
+  methods: {
+    logout() {
+      this.$store.state.commit("LOGOUT");
+      this.isAuthenticated = false;
+    },
+  computed: {
+    isAuthenticated() {
+      return !this.$store.state.token === '';
+    }
+  }
+    }
+
+  
 };
 </script>
