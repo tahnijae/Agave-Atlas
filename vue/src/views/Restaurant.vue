2ef724541fe8c07844843758f8088e6f808fc507
wilson warholic <willwarholic@gmail.com>
2023-04-13 13:32:10 -0400

    fixed the restraunt view

--- vue/src/views/Restaurant.vue
+++ vue/src/views/Restaurant.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
+    <navigation-bar/>
     <drink-list/>
   </div>
 </template>
 
 <script>
 import DrinkList from '../components/DrinkList.vue'
+import NavigationBar from '../components/NavigationBar.vue'
 export default {
-  components: { DrinkList },
+  components: { DrinkList, 
+                NavigationBar},
 
 }
 </script>
