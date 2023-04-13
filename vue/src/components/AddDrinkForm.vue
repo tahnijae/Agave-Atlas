cc76d99b28b84e5620b8905c7edaaa0555856ce0
wilson warholic <willwarholic@gmail.com>
2023-04-13 14:11:28 -0400

    drinks form

---
+++ vue/src/components/AddDrinkForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+      <form>
+
+      </form>
+  </div>
+</template>
+
+<script>
+import drinkService from '../services/DrinkService.vue'
+
+export default {
+name: "drink-form",
+data(){
+  return{
+    drink: {
+      name:'',
+      description:'',
+      isFrozen:false,
+    }
+  }
+},
+methods:{
+  submitForm(){
+    const newDrink = {
+      name : this.name,
+      description: this.description,
+      isFrozen: this.isFrozen
+    }
+    drinkService.addDrink(newDrink).then((response)=>{
+      if(response.status === 200){
+      this.$router.push('/');
+      }
+    })
+  }
+}
+}
+</script>
+
+<style>
+
+</style>
