1da951e9a1af97bc72cc152f36b50000b4086592
Ben Mullaly <benmullaly14gmail.com>
2023-04-13 14:50:37 -0400

    weather component zip code functioning with responsive title and error handling, still needs some css magic

--- vue/src/services/WeatherService.js
+++ vue/src/services/WeatherService.js
@@ -7,5 +7,10 @@ const http = axios.create({
 export default {
 GetWeatherCuyahoga(){
     return http.get('/zones/county/OHZ011/forecast')
-}
+},
+
+async GetWeatherByCoordinate(lat, long){
+    let office = await http.get(`/points/${lat},${long}`)
+    return http.get(`/gridpoints/${office.data.properties.gridId}/${office.data.properties.gridX},${office.data.properties.gridY}/forecast`);
+    }
 }
