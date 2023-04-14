ffaba587bd9956ea99a5d4824abe310cc38fd102
Joseph Francioso <joefrancioso@gmail.com>
2023-04-14 13:34:30 -0400

    Added styling to the socials page

--- vue/src/views/Socials.vue
+++ vue/src/views/Socials.vue
@@ -1,24 +1,39 @@
 <template>
 <div>
+  <navigation-bar />
+  <div v-if="loading" class="loading">
+      <font-awesome-icon :icon="['fas', 'cocktail']" spin />
+    </div>
+  <div class="twitter-div">
     <a class="twitter-timeline" data-lang="en" data-theme="dark" href="https://twitter.com/AgaveAtlas?ref_src=twsrc%5Etfw">Tweets by AgaveAtlas</a> <script type="application/javascript" async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </div>
   </div>
 </template>
 
 <script>
+import NavigationBar from '../components/NavigationBar.vue';
 export default {
+  components: { NavigationBar },
   name: "SocialsPage",
+  data(){
+    return{
+      loading:true
+    }
+  },
+  mounted(){
+    
+  }
 };
 </script>
 
 <style scoped>
-.twitter-feed {
+.twitter-div {
+  margin-top: 5rem;
+}
+.twitter-timeline {
   display: flex;
   justify-content: center;
-  margin-top: 1rem;
-  padding: 1rem;
-  max-width: 100%;
   overflow: hidden;
   text-overflow: ellipsis;
-  height: fit-content(5em);
 }
 </style>
