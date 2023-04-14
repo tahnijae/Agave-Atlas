628bbb1e20550a97b6cd63b8eb931cfcacf70ab6
Joseph Francioso <joefrancioso@gmail.com>
2023-04-14 12:56:23 -0400

    Added socials page and functionality and included an embedded tweet from the twitter account

---
+++ vue/src/views/Socials.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <a class="twitter-timeline" data-lang="en" data-theme="dark" href="https://twitter.com/AgaveAtlas?ref_src=twsrc%5Etfw">Tweets by AgaveAtlas</a> <script type="application/javascript" async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SocialsPage",
+};
+</script>
+
+<style scoped>
+.twitter-feed {
+  display: flex;
+  justify-content: center;
+  margin-top: 1rem;
+  padding: 1rem;
+  max-width: 100%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  height: fit-content(5em);
+}
+</style>
