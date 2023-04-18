5ac7cd22a4dc677f83482cb80847a941ed6e3168
Tahni Hresko <tahni.jae@gmail.com>
2023-04-18 18:54:20 -0400

    got search function in add restaurant form

--- vue/src/components/AddRestaurantForm.vue
+++ vue/src/components/AddRestaurantForm.vue
@@ -2,30 +2,40 @@
   <div class="addRestaurantForm">
     <button
       class="addRestaurant"
-      v-if="!showAddRestaurantForm && isAuthenticated"
+      v-if="!showAddRestaurantForm && isAuthenticated && !hideButton"
       v-on:click="showAddRestaurantForm = !showAddRestaurantForm"
     >
-      Add Restaurant
+      Looking For Somewhere Else?
     </button>
-      <form v-on:submit.prevent="submitForm" v-if="showAddRestaurantForm">
-          <label for="name">Name:</label>
-          <input id="name" type="text" class="input" v-model="restaurant.name" />
-          <label for="city">City:</label>
-          <input id="city" type="text" class="input" v-model="restaurant.city" />
-          <label for="state">State:</label>
-          <input id="state" type="text" class="input" v-model="restaurant.state"/>
-          <label for="state">Zipcode:</label>
-          <input id="state" type="text" class="input" v-model="restaurant.zipCode"/>
-          <button @click.prevent="submitForm">Submit</button>
+    <form v-on:submit.prevent="submitForm" v-if="showAddRestaurantForm">
+      <label for="name">Name:</label>
+      <input id="name" type="text" class="input" v-model="restaurantInput.name" />
+      <label for="city">City:</label>
+      <input id="city" type="text" class="input" v-model="restaurantInput.city" />
+      <label for="state">State:</label>
+      <input id="state" type="text" class="input" v-model="restaurantInput.state" />
+      <label for="zipCode">Zipcode:</label>
+      <input id="zipCode" type="text" class="input" v-model="restaurantInput.zipCode"/>
+      <button @click.prevent="GenerateYelpInfo">Search</button>
 
-          <button  v-on:click="showAddRestaurantForm = !showAddRestaurantForm">Cancel</button>
-      </form>
+      <button v-on:click="showAddRestaurantForm = !showAddRestaurantForm">
+        Cancel
+      </button>
+    </form>
+    <div v-if="haveYelpData">
+      <button>Looks Good!</button>
+      <button v-on:click="showAddRestaurantForm = !showAddRestaurantForm">Try Again</button>
+      <button>Cancel</button>
+      <yelp-component v-bind:restaurant="yelpReturn" />
+    </div>
   </div>
 </template>
 <script>
-import RestaurantService from '../services/RestaurantService';
+import RestaurantService from "../services/RestaurantService";
+import YelpComponent from "./YelpComponent.vue";
+import yelpService from "../services/YelpService.js";
 
-
+//FIX BUTTONS (MAKE METHODS TO SET BOOLS), MAKE ADD METHOD
 
 export default {
   name: "add-new-restaurant",
@@ -35,38 +45,60 @@ export default {
       required: true,
     },
   },
+  components: { YelpComponent },
   data() {
     return {
-      restaurant: {Name: '',
-      City: '',
-      State: '',
-      Zipcode: '',
-      RestaurantID: Number(this.$route.params.id)
+      restaurantInput: {
+        name: "",
+        city: "",
+        state: "",
+        zipCode: "",
+       // RestaurantID: Number(this.$route.params.id),
       },
-showAddRestaurantForm: false,
-    isLoggedIn : false,
+      showAddRestaurantForm: false,
+      hideButton: false,
+      isLoggedIn: false,
+      haveYelpData: false,
+      yelpReturn: {},
     };
   },
   created() {
     this.isLoggedIn = this.$store.state.token !== "";
-},
-methods: {
-  submitForm() {
-    RestaurantService.addRestaurant(this.restaurant).then((response) => {
-      if (response.status === 200) {
-        this.$emit("restaurantAdded");
-        this.showAddRestaurantForm = false;
-        this.restaurant = {
-          Name: "",
-          City: "",
-          State: "",
-          Zipcode: "",
-          RestaurantID: Number(this.$route.params.id),
-        };
-      }
-    });
-  }
-},
+  },
+  methods: {
+    submitForm() {
+      RestaurantService.addRestaurant(this.restaurantInput).then((response) => {
+        if (response.status === 200) {
+          this.$emit("restaurantAdded");
+          this.showAddRestaurantForm = false;
+          this.restaurantInput = {
+            name: "",
+            city: "",
+            state: "",
+            zipCode: "",
+          };
+        }
+      });
+    },
+    GenerateYelpInfo() {
+      let yelpSearch = {
+        name: this.restaurantInput.name,
+        zipcode: this.restaurantInput.city + " " + this.restaurantInput.state + " " + this.restaurantInput.zipCode,
+      };
+      yelpService
+        .GetBusinessByNameAndZip(yelpSearch)
+        .then((response) => {
+          this.yelpReturn = response.data;
+          this.haveYelpData = true;
+          this.showAddRestaurantForm = false;
+          this.hideButton = true;
+          console.log(this.yelpReturn);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
 <style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -5,8 +5,8 @@
       <!-- <yelp-component /> -->
     <div class="restaurant-list">
       <h2>Locations:</h2>
-       <add-restaurant-form :is-authenticated="isAuthenticated" />
       <restaurant-list />
+      <!-- <add-restaurant-form :is-authenticated="isAuthenticated" /> -->
     </div>
      <twitter-feed />
   </div>
@@ -17,7 +17,7 @@ import WeatherOutput from '../components/WeatherOutput.vue';
 import NavigationBar from '../components/NavigationBar.vue'
 import authService from "../services/AuthService.js";
 import TwitterFeed from "../components/TwitterFeed.vue";
-import AddRestaurantForm from '../components/AddRestaurantForm.vue';
+//import AddRestaurantForm from '../components/AddRestaurantForm.vue';
 //import YelpComponent from '../components/YelpComponent.vue';
 
 
@@ -28,7 +28,7 @@ export default {
     WeatherOutput,
     NavigationBar,
     TwitterFeed,
-    AddRestaurantForm,
+//    AddRestaurantForm,
     //YelpComponent,
   },
   data() {
