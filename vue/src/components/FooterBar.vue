cabcf0843773eb1025ecfeb66077d36e46b1c3e2
Joseph Francioso <joefrancioso@gmail.com>
2023-04-20 00:21:03 -0400

    worked on footer

--- vue/src/components/FooterBar.vue
+++ vue/src/components/FooterBar.vue
@@ -1,57 +1,43 @@
 <template>
-  <footer>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-4">
-          <h4>Contact Us</h4>
-          <p>Email: agaveatlas@gmail.com</p>
-          <p>Address: 7100 Euclid Ave #140, Cleveland, OH 44103</p>
-        </div>
-        <div class="row bottom-row">
-        <div class="col-md-12 github-icons">
-          <h4>Follow Us</h4>
-          <ul class="social-icons">
-            <li>
-              <a href="https://twitter.com/AgaveAtlas">
-                <font-awesome-icon :icon="['fab', 'twitter']" />
-              Twitter</a>
-            </li>
-                    <li>
-                        <a href="https://github.com/bmullaly14">
-                            <font-awesome-icon class="fa-lg" :icon="['fab', 'github-alt']" style="color: purple" />
-                        Ben Mullaly</a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/jfrancioso">
-                            <font-awesome-icon class="fa-lg" :icon="['fab', 'github-alt']" style="color: green" />
-                        Joseph Francioso</a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/tahnijae">
-                            <font-awesome-icon class="fa-lg" :icon="['fab', 'github-alt']" style="color: black" />
-                        Tahni Hresko</a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/user4">
-                            <font-awesome-icon class="fa-lg" :icon="['fab', 'github-alt']" style="color: red" />
-                        Wilson Warholic</a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/DaleJMitchell">
-                            <font-awesome-icon class="fa-lg" :icon="['fab', 'github-alt']" style="color: blue" />
-                        Dale Mitchell</a>
-                    </li>
-                </ul>
-        </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-12">
-          <hr>
-          <p class="text-center">&copy; 2023 AgaveAtlas. All rights reserved.</p>
-        </div>
-      </div>
-    </div>
+  <footer class="footer">
+    <ul>
+      <li class="twitter-icon">
+        <a href="https://twitter.com/AgaveAtlas">
+          <font-awesome-icon :icon="['fab', 'twitter']" />
+          Twitter
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/bmullaly14">
+          <font-awesome-icon class="fa-lg" :icon="['fab', 'github-alt']" style="color: purple" />
+          Ben Mullaly
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/jfrancioso">
+          <font-awesome-icon class="fa-lg" :icon="['fab', 'github-alt']" style="color: green" />
+          Joseph Francioso
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/tahnijae">
+          <font-awesome-icon class="fa-lg" :icon="['fab', 'github-alt']" style="color: black" />
+          Tahni Hresko
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/user4">
+          <font-awesome-icon class="fa-lg" :icon="['fab', 'github-alt']" style="color: red" />
+          Wilson Warholic
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/DaleJMitchell">
+          <font-awesome-icon class="fa-lg" :icon="['fab', 'github-alt']" style="color: blue" />
+          Dale Mitchell
+        </a>
+      </li>
+    </ul>
   </footer>
 </template>
 
@@ -63,102 +49,47 @@ import { fab } from "@fortawesome/free-brands-svg-icons";
 library.add(fab);
 
 export default {
-    components: {
-        FontAwesomeIcon
-    },
-    methods: {
-        scrollToTop() {
-            window.scrollTo({ top: 0, behavior: "smooth" });
-        }
-    }
+  name: "FooterBar",
+  components: {
+    FontAwesomeIcon,
+  },
 };
 </script>
 
-<style>
-footer {
-  background-color: #333;
-  color: white;
-  padding: 1em;
-  position: fixed;
+<style scoped>
+.footer {
+  position: absolute;
   bottom: 0;
   left: 0;
   right: 0;
+  background-color: #333;
+  color: white;
+  padding: 1em;
 }
 
-.twitter-button {
-  font-size: 2.5rem;
-}
-
-#footer {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-#footer_name img {
-  max-height: 3em;
-}
-
-#footer_buttons {
+ul {
+  list-style: none;
   display: flex;
   justify-content: center;
-  margin-top: 1em;
+  align-items: center;
+  margin: 0;
+  padding: 0;
 }
 
-#footer_buttons button {
-  background-color: transparent;
-  border: none;
+li {
+  margin-right: 1em;
+}
+
+a {
+  text-decoration: none;
   color: white;
-  cursor: pointer;
-  font-size: 1rem;
-  margin-left: 1em;
 }
 
-#footer_buttons button:hover {
-  color: #ccc;
+a:hover {
+  text-decoration: underline;
 }
 
-
-  /* github styles ... */
-  
-    .bottom-row {
-    margin-top: 1em;
-  }
-
-  .github-icons {
-    display: flex;
-    justify-content: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .github-icons ul {
-    display: flex;
-    list-style-type: none;
-    padding: 0;
-    margin: 0;
-  }
-
-  .github-icons li {
-    margin-right: 2em; /* Add a right margin to create space between items */
-  }
-
-  .github-icons li:last-child {
-    margin-right: 0; /* Remove right margin from the last item */
-  }
-
-  .github-icons li a {
-    display: flex;
-    align-items: center;
-    text-decoration: none;
-    color: black;
-  }
-
-  .github-icons li a:hover {
-    text-decoration: underline;
-  }
-
-  .github-icons li a font-awesome-icon {
-    margin-right: 10px; /* increase the margin-right value */
-  }
-
+.twitter-icon {
+  margin-right: 1em;
+}
 </style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -9,7 +9,11 @@
       <!-- <add-restaurant-form :is-authenticated="isAuthenticated" /> -->
     </div>
      <twitter-feed />
+     <footer style="grid-area: footer; grid-row: 4; grid-column: 2/5;">
+       <div class="footer-content">
      <footer-bar />
+       </div>
+     </footer>
   </div>
 </template>
 <script>
@@ -18,7 +22,7 @@ import WeatherOutput from '../components/WeatherOutput.vue';
 import NavigationBar from '../components/NavigationBar.vue'
 import authService from "../services/AuthService.js";
 import TwitterFeed from "../components/TwitterFeed.vue";
-import FooterBar from '../components/FooterBar.vue';
+// import FooterBar from '../components/FooterBar.vue';
 //import AddRestaurantForm from '../components/AddRestaurantForm.vue';
 //import YelpComponent from '../components/YelpComponent.vue';
 
@@ -30,7 +34,7 @@ export default {
     WeatherOutput,
     NavigationBar,
     TwitterFeed,
-    FooterBar,
+    // FooterBar,
 //    AddRestaurantForm,
     //YelpComponent,
   },
@@ -49,17 +53,6 @@ export default {
       this.$store.commit("LOGOUT");
       this.isAuthenticated = false;
     }
-  },
- mounted() {
-    window.addEventListener("scroll", () => {
-    const main = this.$refs.main;
-    const footer = document.querySelector("footer");
-    if (main && main.offsetHeight + main.offsetTop < window.innerHeight + window.pageYOffset) {
-      footer.style.opacity = 1;
-    } else {
-      footer.style.opacity = 0;
-    }
-    });
   },
 };
 </script>
@@ -87,14 +80,15 @@ export default {
   
 }
 
-.main{
+.main {
   display: grid;
   grid-template-areas: 
-  "banner banner banner banner banner"
-  ". weather weather weather ."
-  ". rest rest rest ."
-  ". footer footer footer ."; /* Add footer to grid */
+    "banner banner banner banner banner"
+    ". weather weather weather ."
+    ". rest rest rest ."
+    ". footer footer footer .";
   grid-template-columns: .5fr 1fr 4fr 1fr .5fr;
+  grid-template-rows: auto auto 1fr auto; /* Add this line */
   background-color: #def7d0;
   /* background-image: url('/src/assets/cheers.jpg'); */
   background-size: cover;
@@ -102,21 +96,22 @@ export default {
   background-repeat:no-repeat;
   background-attachment: fixed;
   font-family: Verdana, Geneva, Tahoma, sans-serif;
-  padding-bottom: 20em; /* add padding to bottom */
-
 }
-footer {
-  background-color: #F3FAEF;
+
+/* footer {
+  background-color: #f3faef;
   color: rgb(24, 23, 23);
   padding: 1em;
-  transition: opacity 0.3s ease-out;
-  opacity: 0;
-  transition-delay: 0.5s;
-}
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+} */
 
-footer.show {
-  opacity: 1;
-}
+
+
+
 h2{
   color: Black;
 }
